115bebac844c91bb88bc00b4f3a4f0c254442467
Nik Di <nikoladimitrov012@gmail.com>
2021-05-23 21:50:37 +0300

    new file:   .idea/.idea.CpS-Counter-Timer/.idea/.gitignore 	new file:   .idea/.idea.CpS-Counter-Timer/.idea/discord.xml 	new file:   .idea/.idea.CpS-Counter-Timer/.idea/encodings.xml 	new file:   .idea/.idea.CpS-Counter-Timer/.idea/indexLayout.xml 	new file:   .idea/.idea.CpS-Counter-Timer/.idea/riderModule.iml 	new file:   .vs/CpS-Counter-Timer/v15/.suo 	new file:   .vs/CpS-Counter-Timer/v15/Server/sqlite3/db.lock 	new file:   .vs/CpS-Counter-Timer/v15/Server/sqlite3/storage.ide 	new file:   .vs/CpS-Counter-Timer/v15/Server/sqlite3/storage.ide-shm 	new file:   .vs/CpS-Counter-Timer/v15/Server/sqlite3/storage.ide-wal 	new file:   .vs/CpS-Counter-Timer/v16/.suo 	new file:   CpS-Counter-Timer.sln 	new file:   CpS-Counter-Timer/App.config 	new file:   CpS-Counter-Timer/CpS-Counter-Timer.csproj 	new file:   CpS-Counter-Timer/Form1.Designer.cs 	new file:   CpS-Counter-Timer/Form1.cs 	new file:   CpS-Counter-Timer/Form1.resx 	new file:   CpS-Counter-Timer/Program.cs 	new file:   CpS-Counter-Timer/Properties/AssemblyInfo.cs 	new file:   CpS-Counter-Timer/Properties/Resources.Designer.cs 	new file:   CpS-Counter-Timer/Properties/Resources.resx 	new file:   CpS-Counter-Timer/Properties/Settings.Designer.cs 	new file:   CpS-Counter-Timer/Properties/Settings.settings 	new file:   CpS-Counter-Timer/bin/Debug/CpS-Counter-Timer.exe 	new file:   CpS-Counter-Timer/bin/Debug/CpS-Counter-Timer.exe.config 	new file:   CpS-Counter-Timer/bin/Debug/CpS-Counter-Timer.pdb 	new file:   CpS-Counter-Timer/obj/Debug/.NETFramework,Version=v4.6.1.AssemblyAttributes.cs 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.csproj.CoreCompileInputs.cache 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.csproj.FileListAbsolute.txt 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.csproj.GenerateResource.cache 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.csprojAssemblyReference.cache 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.exe 	new file:   CpS-Counter-Timer/obj/Debug/CpS-Counter-Timer.pdb 	new file:   CpS-Counter-Timer/obj/Debug/CpS_Counter_Timer.Form1.resources 	new file:   CpS-Counter-Timer/obj/Debug/CpS_Counter_Timer.Properties.Resources.resources 	new file:   CpS-Counter-Timer/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   CpS-Counter-Timer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   CpS-Counter-Timer/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   CpS-Counter-Timer/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   CpS-Counter-Timer/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ CpS-Counter-Timer/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CpS_Counter_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 26.25F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.Location = new System.Drawing.Point(13, 226);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1238, 222);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Here!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Left:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(821, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clicks per Second (CpS): ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(483, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 46);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Clicks:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 26.25F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button2.Location = new System.Drawing.Point(1098, 164);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(150, 29);
+            this.label4.MaximumSize = new System.Drawing.Size(57, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 40);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(576, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 41);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(1137, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 41);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1264, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(9);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CpS Counter with Timer";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CpS-Counter-Timer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpS_Counter_Timer
+{
+    public partial class Form1 : Form
+    {
+        int _clicks = 0;
+        double _time = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(_time > 0)
+            {
+                _clicks++;
+                label5.Text = _clicks.ToString();
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_time > 0)
+            {
+                _time -= 0.1;
+                label4.Text = _time.ToString();
+            }
+            else
+            {
+                timer1.Stop();
+                label6.Text = ((double)_clicks/10).ToString();
+            }
+            if (_time <= 7.0)
+            {
+                label4.Location = new Point(150, 30);
+            }
+            if (label4.Text.Contains("-0"))
+            {
+                label4.Text = "0";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _time = 10;
+            _clicks = 0;
+            label5.Text = "0";
+            label6.Text = "0";
+            timer1.Stop();
+            label4.Text = _time.ToString();
+            label4.Location = new Point(150, 29);
+        }
+    }
+}
